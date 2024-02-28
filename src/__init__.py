3bcf9ae368e87a0242e5fb5569e539153dcd5677
Kevin zhang <96759764+Dreammob@users.noreply.github.com>
2024-02-27 20:40:36 -0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,21 +1,7 @@
-import cv2
-import numpy as np
-import mediapipe as mp
-import traceback
-from .utils import (
-    get_landmark_coordinates,
-    calculate_angle,
-    log_landmark,
-    log_angle,
-    calculate_slope,
-    compare_nums,
-)
-from .arm import ArmsState
-from .leg import LegsState
-from .face import FaceState
-from .events import Events
-from .const import IMAGE_HEIGHT, IMAGE_WIDTH, DRIVING_UP_AREA
+# This file intilizes the executable
 
-mp_drawing = mp.solutions.drawing_utils
-mp_drawing_styles = mp.solutions.drawing_styles
-mp_pose = mp.solutions.pose
+
+#from .capture import 
+#from .detector import 
+#from .mapper import
+#from .integrator import 
