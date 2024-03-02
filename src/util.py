6dcd95f79f26d5f67beb4ba353a4aa64b5701b53
Kevin zhang <96759764+Dreammob@users.noreply.github.com>
2024-03-02 12:19:25 -0800

    Update util.py

--- src/util.py
+++ src/util.py
@@ -1,1 +1,20 @@
 # Contains utility functions used across the project.
+
+import cv2
+import mediapipe as mp
+import numpy as np
+
+
+def calculate_angle(a,b,c):
+    # returns the joint angles between points
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    rads = np.arctan2(c[1]-b[1], c[0]-b[0]) - np.arctan2(a[1]-b[1], a[0]-b[0])
+    angle = np.abs(rads*180.0/np.pi)
+    if angle > 180.0:
+        angle = 360.0 - angle 
+    return angle
+
+
