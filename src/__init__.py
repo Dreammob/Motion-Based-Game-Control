5c2c20d588078c69a780e8056e93f607f6ba075d
Kevin zhang <96759764+Dreammob@users.noreply.github.com>
2024-03-02 00:23:05 -0800

    mediapipe working live

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,52 @@
-# This file intilizes the executable
+import cv2
+import mediapipe as mp
+import numpy as np
 
+# Initialize MediaPipe Pose
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
 
-#from .capture import 
-#from .detector import 
-#from .mapper import
-#from .integrator import 
+# predefined pose classifier
+#pose_classifier = PoseClassifier()
+
+cap = cv2.VideoCapture(0)
+## Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        
+        # Recolor image to RGB
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+      
+        # Make detection
+        results = pose.process(image)
+    
+        # Recolor back to BGR
+        image.flags.writeable = True
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        
+        # Extract landmarks
+        try:
+            landmarks = results.pose_landmarks.landmark
+            print(landmarks)
+        except:
+            pass
+        
+        
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)               
+        
+        cv2.imshow('Mediapipe Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+for lndmrk in mp_pose.PoseLandmark:
+    print(lndmrk)
+landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].visibility
+landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value]
+landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value]
